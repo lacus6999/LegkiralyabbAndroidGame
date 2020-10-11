1a6b8e75697cb4f989d8aab4afe0d3f1b9bd14e2
kamu.laszlo <kamu.laszlo@promera.hu>
2020-10-11 16:58:21 +0200

    bluetooth get paired device

--- app/src/main/java/com/bluetooth/BluetoothActivity.java
+++ app/src/main/java/com/bluetooth/BluetoothActivity.java
@@ -2,14 +2,18 @@ package com.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.MainActivity;
 import com.example.game.R;
+import com.game.GameActivity;
 
 import java.util.Set;
 
@@ -31,15 +35,26 @@ public class BluetoothActivity extends AppCompatActivity {
         if(bluetoothAdapter != null){
             if(bluetoothAdapter.isEnabled()){
                 Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                if(!pairedDevices.isEmpty()){
+                    TextView textView = findViewById(R.id.paired_device);
+                    BluetoothDevice bluetoothDevice = pairedDevices.iterator().next();
+                    textView.setText(bluetoothDevice.getName());
 
-
-                LinearLayout bluetoothAvailableDevices = findViewById(R.id.bluetooth_available_devices);
-                TextView textView = new TextView(this);
-                textView.setText("kacsa");
-                bluetoothAvailableDevices.addView(textView);
+                    final Button button = findViewById(R.id.bluetooth_continue);
+                    button.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            startActivity(new Intent(BluetoothActivity.this, GameActivity.class));
+                        }
+                    });
+                }
+                else {
+                    TextView textView = findViewById(R.id.paired_device);
+                    textView.setText("No paired device.");
+                }
             }
+            //setTextView();
+        } else {
+            //setTextView("Bluetooth is not accessible on your device.");
         }
-
-
     }
 }
